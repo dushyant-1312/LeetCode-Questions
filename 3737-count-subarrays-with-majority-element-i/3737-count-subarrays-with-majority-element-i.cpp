73a2e935f80291f9c66236fe26e3b08df1e45b16
DUSHYANT <dushyantmudgal1312@gmail.com>
2026-06-25 11:33:20 +0530

    Time: 50 ms (40.09%), Space: 39.6 MB (57.52%) - LeetHub

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
@@ -13,19 +13,20 @@ public:
         return false;
     }
     int countMajoritySubarrays(vector<int>& nums, int target) {
-        int count = 0;
+        int ans = 0;
 
         for(int i=0; i<nums.size(); i++){
+            int  count = 0; int mark = 0; 
             for(int j=i; j<nums.size(); j++){
-                if(check(nums,i,j,target)) count++;
+                if(nums[j] == target){
+                    count++;
+                }
+                mark++;
+                if(count > mark/2){
+                    ans++;
+                }
             }
         }
-
-
-        // temp
-        // for(int i=0; i<nums.size(); i++){
-
-        // }
-        return count;
+        return ans;
     }
 };
