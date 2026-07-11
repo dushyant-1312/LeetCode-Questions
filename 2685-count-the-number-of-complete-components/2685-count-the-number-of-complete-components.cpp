388dbb15b920526ab782ab3920c16028f3c47c6e
DUSHYANT <dushyantmudgal1312@gmail.com>
2026-07-11 17:09:25 +0530

    Time: 60 ms (37.94%), Space: 132.7 MB (51.08%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -1,31 +1,31 @@
 class Solution {
-private: 
-    void dfscheck(int node,vector<int> &vis,vector<int> adj[],int &nodecount,int &edgecount){
-        vis[node] = 1; 
-        nodecount++;
-        for(auto it : adj[node]){
-            edgecount++;
+public:
+    void dfscheck(int node, vector<int> &vis, vector<int> graph[], int &countedge, int &countnode){
+        vis[node] = 1;
+        countnode++;
+        for(auto it : graph[node]){
+            countedge++;
             if(!vis[it]){
-                dfscheck(it,vis,adj,nodecount,edgecount);
+                dfscheck(it, vis, graph, countedge, countnode);
             }
         }
     }
-public:
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        vector<int> adj[n];
-        vector<int> vis(n,0);
-        for(auto  it : edges){
-            adj[it[0]].push_back(it[1]);
-            adj[it[1]].push_back(it[0]);
+        vector<int> graph[n];
+        for(auto it : edges){
+            graph[it[0]].push_back(it[1]);
+            graph[it[1]].push_back(it[0]);
         }
-        int count=0;
-        for(int i=0;i<n;i++){
-            int nodecount = 0; int edgecount=0;
+        vector<int> vis(n, 0);
+
+        int ans = 0;
+        for(int i=0; i<n; i++){
             if(!vis[i]){
-                dfscheck(i,vis,adj,nodecount,edgecount);
-                if(nodecount*(nodecount-1) == edgecount) count++;
+            int countnode = 0; int countedge = 0;
+                dfscheck(i, vis, graph, countedge, countnode);
+                if(countedge == countnode * (countnode-1)) ans++;
             }
         }
-        return count;
+        return ans;
     }
 };
