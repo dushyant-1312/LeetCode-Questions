c3642cb603141d66b6ae20a9837afb995bb666b5
DUSHYANT <dushyantmudgal1312@gmail.com>
2026-09-09 17:54:17 +0530

    Time: 4 ms (1.33%), Space: 9.1 MB (57.33%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -1,56 +1,14 @@
-// class Solution {
-// public:
-//     long long countCommas(long long n) {
-//         if(n < 1000) return 0;
-
-//         if(n == 1e15) return 5 + (1e15-1  - 1e12 + 1) * 4 + (1e12- 1e9 + 1) *3 +  (1e9- 1e6 + 1) * 2 +  (1e9- 1e3 + 1);  
-//         else if(n >= 1e12) {
-//             return  (n - 1e12 + 1) * 4 + (1e12- 1e9 + 1) *3 +  (1e9- 1e6 + 1) * 2 +  (1e9- 1e3 + 1);  
-//         }
-//         else if(n >= 1e9){
-//             return (n- 1e9 + 1) *3 +  (1e9- 1e6 + 1) * 2 +  (1e9- 1e3 + 1);  
-//         }
-//         else if(n >= 1e6){
-//             return (n - 1e6 + 1) * 2 +  (1e9- 1e3 + 1);  
-//         }
-//         else {
-//             return (n- 1e3 + 1);  
-//         }
-//         return -1;
-//     }
-// };
 class Solution {
 public:
     long long countCommas(long long n) {
         if(n < 1000) return 0;
 
         if(n == 1e15)
-            return 5 + (1e15 - 1 - 1e12 + 1) * 4
-                     + (1e12 - 1e9) * 3
-                     + (1e9 - 1e6) * 2
-                     + (1e6 - 1e3);
-
-        else if(n >= 1e12) {
-            return (n - 1e12 + 1) * 4
-                 + (1e12 - 1e9) * 3
-                 + (1e9 - 1e6) * 2
-                 + (1e6 - 1e3);
-        }
-
-        else if(n >= 1e9) {
-            return (n - 1e9 + 1) * 3
-                 + (1e9 - 1e6) * 2
-                 + (1e6 - 1e3);
-        }
-
-        else if(n >= 1e6) {
-            return (n - 1e6 + 1) * 2
-                 + (1e6 - 1e3);
-        }
-
-        else {
-            return (n - 1e3 + 1);
-        }
+            return 5 + (1e15 - 1 - 1e12 + 1) * 4 + (1e12 - 1e9) * 3 + (1e9 - 1e6) * 2 + (1e6 - 1e3);
+        else if(n >= 1e12) return (n - 1e12 + 1) * 4 + (1e12 - 1e9) * 3 + (1e9 - 1e6) * 2 + (1e6 - 1e3);
+        else if(n >= 1e9) return (n - 1e9 + 1) * 3 + (1e9 - 1e6) * 2 + (1e6 - 1e3);
+        else if(n >= 1e6) return (n - 1e6 + 1) * 2 + (1e6 - 1e3);
+        else return (n - 1e3 + 1);
 
         return -1;
     }
