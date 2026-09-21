80bb5ab9d60f11856309965fd758011a6069dc29
DUSHYANT <dushyantmudgal1312@gmail.com>
2026-09-21 17:13:42 +0530

    Time: 223 ms (62.63%), Space: 10.6 MB (56.25%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,49 @@
+int arrRow[] = {1, 0, -1, 0}, arrColum[] = {0, -1, 0, 1};
+
+class Solution {
+public:
+
+    bool backTracking(int row, int colum, int index, vector<vector<int>>&visits, vector<vector<char>>& board, string&word){
+
+        bool ans = false;
+
+        if(index == word.length()){
+            return true;
+        }
+
+            
+            visits[row][colum] = 1;
+            int i, auxRow, auxColum;
+
+            for(i = 0; i<4 && !ans; i++){
+
+                auxRow = row + arrRow[i];
+                auxColum = colum + arrColum[i];
+
+                if(auxRow != -1 && auxColum != -1 && auxRow != board.size() && auxColum != board[0].size() && !visits[auxRow][auxColum] && board[auxRow][auxColum] == word[index]){
+                    ans = backTracking(auxRow, auxColum, index+1, visits, board, word);
+                }
+            }
+            visits[row][colum] = 0;
+
+
+        return ans;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        
+        int i, j;
+        bool ans = false;
+        vector<vector<int>>visits(board.size(), vector<int>(board[0].size(), 0));
+
+        for(i = 0; i<board.size() && !ans; i++){
+            for(j = 0; j<board[0].size() && !ans; j++){
+                if(board[i][j] == word[0]){
+                    ans = backTracking(i, j, 1, visits, board, word);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
